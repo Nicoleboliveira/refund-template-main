39ca00c1789d1ad40c63c5ab01acad0bb5b2ccae
nicole <nicoleboliveira14@gmail.com>
2025-03-17 14:56:18 -0300

    Obtendo os dados do formulário

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //Seleciona os elementos do formulário.
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 //Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -21,3 +24,7 @@ function formatCurrencyBRL(value) {
 
   return value;
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+};
