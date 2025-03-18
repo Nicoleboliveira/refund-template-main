cf9147d35b5a9b007d479287acfc4d9530a39c52
nicole <nicoleboliveira14@gmail.com>
2025-03-18 09:41:26 -0300

    Criando o ícone de remover

--- scripts.js
+++ scripts.js
@@ -77,8 +77,14 @@ function expenseAdd(newExpense) {
       .toUpperCase()
       .replace("R$", "")}`;
 
+    //Cria o ícone de remover
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+
     //Adiciona as informações no item.
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
     //adiciona o item na lista.
     expenseList.append(expenseItem);
   } catch (error) {
